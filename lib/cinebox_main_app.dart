e4f87635d542b8dcd1553558f7488e0aa28deb64
morpa <andremorpa@gmail.com>
2025-08-12 11:31:29 +0100

    feat: add splash screen and initial project setup

---
+++ lib/cinebox_main_app.dart
@@ -0,0 +1,17 @@
+import 'package:cinebox/ui/core/themes/theme.dart';
+import 'package:cinebox/ui/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CineboxMainApp extends StatelessWidget {
+  const CineboxMainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CineBox',
+      theme: AppTheme.theme,
+      routes: {'/': (_) => SplashScreen()},
+    );
+  }
+}
