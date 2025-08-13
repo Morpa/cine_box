162a144c37cfc0b108cda0812583793fa9297468
morpa <andremorpa@gmail.com>
2025-08-13 16:41:29 +0100

    feat: enhance login screen with background image and Google Sign-In button

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/login/widgets/sign_in_google_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,10 +14,33 @@ class _State extends ConsumerState<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Login Screen',
-        ),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            padding: const EdgeInsets.only(top: 108),
+            child: Column(
+              spacing: 48,
+              children: [
+                Image.asset(R.ASSETS_IMAGES_LOGO_PNG),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: SignInGoogleButton(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
